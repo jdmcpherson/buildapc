c731a8f5f806a3d4afe745c25bc59bc43bc1bece
James McPherson <mcpherson.jamesd@gmail.com>
2018-03-23 16:21:45 -0700

    Initialized node.js project, ported over previous progress

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+
+app.use(logger('dev'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+   res.render('index');
+});
+
+app.get('/build', function (req, res) {
+   res.render('build');
+});
+
+app.get('/products', function (req, res) {
+   res.render('products');
+});
+
+app.get('/guides', function (req, res) {
+   res.render('guides');
+});
+
+app.get('/submitted', function (req, res) {
+   res.render('submitted');
+});
+
+const DOMAIN = 'localhost';
+const PORT = '8080';
+app.listen(PORT, DOMAIN, () => {
+   console.log(`🖥 Server listenning on http://${DOMAIN}:${PORT}`);
+});
